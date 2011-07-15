f9ac8fd1d315aedf7784f9f522e8ac6b0554ffdb
Berend Scholte <berend@berendscholte.nl>
2011-07-15 20:13:09 +0200

    Change: Use EnumMap.

--- src/main/java/nl/alleveenstra/genyornis/sessions/Session.java
+++ src/main/java/nl/alleveenstra/genyornis/sessions/Session.java
@@ -1,22 +1,35 @@
 package nl.alleveenstra.genyornis.sessions;
 
+import java.util.EnumMap;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+enum AntStatus {
+  INITIALIZING,
+  COMPILING,
+  COPYING,
+  JARRING,
+  ZIPPING,
+  DONE,
+  ERROR
+}
 /**
  * @author alle.veenstra@gmail.com
  */
 public class Session {
     private static final Logger log = LoggerFactory.getLogger(Session.class);
 
+	EnumMap<TYPES, Object> values = new EnumMap<TYPES, Object>(TYPES.class);
+	public Session() {
+		values.put(TYPES.REQUEST_COUNTER, null);
+	}
+
     public static enum TYPES {
         REQUEST_COUNTER
     }
-
-    private Map<TYPES, Object> values = new HashMap<TYPES, Object>();
 
     public <T> T getValue(TYPES type) {
         if (values.containsKey(type))
