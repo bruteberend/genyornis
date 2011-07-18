d9fc7c1c94c28ebce7ea6b3590cb9403ab3e8892
Berend Scholte <berend@berendscholte.nl>
2011-07-18 21:56:17 +0200

    Change: Layout to coding standard.

--- src/main/java/nl/alleveenstra/genyornis/javascript/ApplicationPool.java
+++ src/main/java/nl/alleveenstra/genyornis/javascript/ApplicationPool.java
@@ -12,71 +12,70 @@ import org.slf4j.LoggerFactory;
  * @author alle.veenstra@gmail.com
  */
 public class ApplicationPool {
-    private static final Logger log = LoggerFactory.getLogger(ApplicationPool.class);
 
-    private static final String SCRIPTFOLDER = "/javascripts";
-    private static final String USER_DIR = "user.dir";
-    private static ApplicationPool instance;
+	private static final Logger log = LoggerFactory.getLogger(ApplicationPool.class);
+	private static final String SCRIPTFOLDER = "/javascripts";
+	private static final String USER_DIR = "user.dir";
+	private static ApplicationPool instance;
+	private Map<String, Application> apps;
+	private WatchDog watchDog;
 
-    private Map<String, Application> apps;
-    private WatchDog watchDog;
+	private ApplicationPool() {
+		apps = new HashMap<String, Application>();
+		watchDog = new WatchDog(this);
+		watchDog.start();
+	}
 
-    private ApplicationPool() {
-        apps = new HashMap<String, Application>();
-        watchDog = new WatchDog(this);
-        watchDog.start();
-    }
+	public static ApplicationPool getInstance() {
+		if (instance == null) {
+			instance = new ApplicationPool();
+		}
+		return instance;
+	}
 
-    public static ApplicationPool getInstance() {
-        if (instance == null) {
-            instance = new ApplicationPool();
-        }
-        return instance;
-    }
+	public WatchDog getWatchDog() {
+		return watchDog;
+	}
 
-    public WatchDog getWatchDog() {
-        return watchDog;
-    }
+	public void start(String appname) {
+		Thread app = apps.get(appname);
+		if (app != null) {
+			app.start();
+		}
+	}
 
-    public void start(String appname) {
-        Thread app = apps.get(appname);
-        if (app != null) {
-            app.start();
-        }
-    }
+	public void stop(String appname) {
+		Thread app = apps.get(appname);
+		if (app != null) {
+			app.interrupt();
+		}
+	}
 
-    public void stop(String appname) {
-        Thread app = apps.get(appname);
-        if (app != null) {
-            app.interrupt();
-        }
-    }
+	public Collection<Application> list() {
+		return apps.values();
+	}
 
-    public Collection<Application> list() {
-        return apps.values();
-    }
+	public long getCPUTime(String applicationName) {
+		if (apps.containsKey(applicationName)) {
+			Application application = apps.get(applicationName);
+			application.updateCpuUsage();
+			return application.getCpuPerSecond();
+		}
+		return -1;
+	}
 
-    public long getCPUTime(String applicationName) {
-        if (apps.containsKey(applicationName)) {
-            Application application = apps.get(applicationName);
-            application.updateCpuUsage();
-            return application.getCpuPerSecond();
-        }
-        return -1;
-    }
-
-    public void deployDirectory(String dirname) {
-        File directory = new File(dirname);
-        if (directory.exists()) {
-            String[] files = directory.list();
-            for (String file : files) {
-                if (!file.startsWith(".")) {
-                    File script = new File(directory.getAbsolutePath().concat("/").concat(file));
-                    Application app = new Application(script);
-                    apps.put(file.toString(), app);
-                    app.start();
-                }
-            }
-        }
-    }
+	public void deployDirectory(String dirname) {
+		File directory = new File(dirname);
+		if (directory.exists()) {
+			String[] files = directory.list();
+			for (String file : files) {
+				if (!file.startsWith(".")) {
+					File script = new File(directory.getAbsolutePath().concat("/").concat(file));
+					Application app = new Application(script);
+					apps.put(file.toString(), app);
+					app.start();
+				}
+			}
+		}
+	}
 }
