12ee9888f4a22bb5361e65999dd005d0a91e8dcc
Berend Scholte <berend@berendscholte.nl>
2011-07-18 21:57:39 +0200

    Change: Layout to coding standard.

--- src/main/java/nl/alleveenstra/genyornis/javascript/Application.java
+++ src/main/java/nl/alleveenstra/genyornis/javascript/Application.java
@@ -22,114 +22,112 @@ import org.slf4j.LoggerFactory;
  * @author alle.veenstra@gmail.com
  */
 public class Application extends Thread {
-    private static final Logger log = LoggerFactory.getLogger(Application.class);
 
-    private static final int EVALUATE_TIMEOUT = 100;
-    private Context cx;
-    MyFactory contextFactory = new MyFactory();
-    private Scriptable scope;
-    private File javascript;
-    private Queue<String> messages = new LinkedList<String>();
+	private static final Logger log = LoggerFactory.getLogger(Application.class);
+	private static final int EVALUATE_TIMEOUT = 100;
+	private Context cx;
+	MyFactory contextFactory = new MyFactory();
+	private Scriptable scope;
+	private File javascript;
+	private Queue<String> messages = new LinkedList<String>();
+	private long cpuPerSecond = 0;
+	private long lastUptime = 0;
+	private long lastThreadCpuTime = 0;
+	private boolean running = true;
 
-    private long cpuPerSecond = 0;
-    private long lastUptime = 0;
-    private long lastThreadCpuTime = 0;
+	public Application(File javascript) {
+		this.javascript = javascript;
+	}
 
-    private boolean running = true;
-
-    public Application(File javascript) {
-        this.javascript = javascript;
-    }
-
-    /**
-     * Run the JavaScript file.
-     */
-
+	/**
+	 * Run the JavaScript file.
+	 */
 	@Override
-    public void run() {
-        cx = contextFactory.enterContext();
-        cx.setOptimizationLevel(-1);
-        cx.setMaximumInterpreterStackDepth(24);
-        scope = cx.initStandardObjects();
+	public void run() {
+		cx = contextFactory.enterContext();
+		cx.setOptimizationLevel(-1);
+		cx.setMaximumInterpreterStackDepth(24);
+		scope = cx.initStandardObjects();
 
-        // make the communication channel available in the scope
-        java.lang.Object wrappedPipe = Context.javaToJS(Genyornis.channelManager(), scope);
-        ScriptableObject.putProperty(scope, "pipe", wrappedPipe);
+		// make the communication channel available in the scope
+		java.lang.Object wrappedPipe = Context.javaToJS(Genyornis.channelManager(), scope);
+		ScriptableObject.putProperty(scope, "pipe", wrappedPipe);
 
-        // make this application available in this scope
-        java.lang.Object wrappedApplication = Context.javaToJS(this, scope);
-        ScriptableObject.putProperty(scope, "application", wrappedApplication);
+		// make this application available in this scope
+		java.lang.Object wrappedApplication = Context.javaToJS(this, scope);
+		ScriptableObject.putProperty(scope, "application", wrappedApplication);
 
-        try {
-            cx.evaluateReader(scope, new FileReader(javascript), javascript.getName(), 0, null);
-            while (running) {
-                try {
-                    String message = getMessage();
-                    cx.evaluateString(scope, message, "<cmd>", 1, null);
-                    sleep(EVALUATE_TIMEOUT);
-                } catch (Exception e) {
-                    log.error(e.getLocalizedMessage());
-	            }
-            }
-        } catch (FileNotFoundException e) {
-            log.error(e.getLocalizedMessage());
-        } catch (IOException e) {
+		try {
+			cx.evaluateReader(scope, new FileReader(javascript), javascript.getName(), 0, null);
+			while (running) {
+				try {
+					String message = getMessage();
+					cx.evaluateString(scope, message, "<cmd>", 1, null);
+					sleep(EVALUATE_TIMEOUT);
+				} catch (Exception e) {
+					log.error(e.getLocalizedMessage());
+				}
+			}
+		} catch (FileNotFoundException e) {
 			log.error(e.getLocalizedMessage());
-        } catch(EvaluatorException e) {
-            // Lol exit!
-        }
-    }
+		} catch (IOException e) {
+			log.error(e.getLocalizedMessage());
+		} catch (EvaluatorException e) {
+			// Lol exit!
+		}
+	}
 
-    /**
-     * Read one message from the queue.
-     *
-     * @return a message
-     * @throws InterruptedException
-     */
-    public synchronized String getMessage() throws InterruptedException {
-        notify();
-        while (messages.isEmpty())
-            wait();
-        String message = (String) messages.poll();
-        return message;
-    }
+	/**
+	 * Read one message from the queue.
+	 *
+	 * @return a message
+	 * @throws InterruptedException
+	 */
+	public synchronized String getMessage() throws InterruptedException {
+		notify();
+		while (messages.isEmpty()) {
+			wait();
+		}
+		String message = (String) messages.poll();
+		return message;
+	}
 
-    /**
-     * Deliver a message to the JavaScript application by calling a function with the message and sender as parameters.
-     *
-     * @param callback
-     * @param from
-     * @param message
-     */
-    public synchronized void deliver(String callback, String from, String message) {
-        String code = callback + "('" + from.replace("'", "\'") + "','" + message.replace("'", "\'") + "')";
-        messages.add(code);
-        notify();
-    }
+	/**
+	 * Deliver a message to the JavaScript application by calling a function with the message and sender as parameters.
+	 *
+	 * @param callback
+	 * @param from
+	 * @param message
+	 */
+	public synchronized void deliver(String callback, String from, String message) {
+		String code = callback + "('" + from.replace("'", "\'") + "','" + message.replace("'", "\'") + "')";
+		messages.add(code);
+		notify();
+	}
 
-    public void updateMemoryUsage() {
-        Object[] ids = scope.getIds();
-        for (Object id : ids) {
-           // scope.get(id)
-        }
-    }
+	public void updateMemoryUsage() {
+		Object[] ids = scope.getIds();
+		for (Object id : ids) {
+			// scope.get(id)
+		}
+	}
 
-    public void updateCpuUsage() {
-        ThreadMXBean mxThread = ManagementFactory.getThreadMXBean();
-        RuntimeMXBean mxRuntime = ManagementFactory.getRuntimeMXBean();
-        long threadCpuTime = mxThread.getThreadCpuTime(getId());
-        long uptime = mxRuntime.getUptime();
-        cpuPerSecond = (threadCpuTime - lastThreadCpuTime) / (uptime - lastUptime);
-        lastUptime = uptime;
-        lastThreadCpuTime = threadCpuTime;
-    }
+	public void updateCpuUsage() {
+		ThreadMXBean mxThread = ManagementFactory.getThreadMXBean();
+		RuntimeMXBean mxRuntime = ManagementFactory.getRuntimeMXBean();
+		long threadCpuTime = mxThread.getThreadCpuTime(getId());
+		long uptime = mxRuntime.getUptime();
+		cpuPerSecond = (threadCpuTime - lastThreadCpuTime) / (uptime - lastUptime);
+		lastUptime = uptime;
+		lastThreadCpuTime = threadCpuTime;
+	}
 
-    public long getCpuPerSecond() {
-        return cpuPerSecond;
-    }
+	public long getCpuPerSecond() {
+		return cpuPerSecond;
+	}
 
-    public void gracefullyQuit() {
-        running = false;
-        contextFactory.gracefullyQuit();
-    }
+	public void gracefullyQuit() {
+		running = false;
+		contextFactory.gracefullyQuit();
+	}
 }
